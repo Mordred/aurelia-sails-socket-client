5a3cece2ab1e5d2d1eaff6095a395f39bc530ac9
Martin Jantosovic <jantosovic.martin@gmail.com>
2015-04-17 22:01:36 +0200

    Fixed lint warnings and version numbers

--- src/transformers.js
+++ src/transformers.js
@@ -20,7 +20,7 @@ export function CSRFTransformerFactory(uri) {
           setCsrfToken(message, client.CSRFToken);
           resolve();
         }).catch(reject);
-      })
+      });
     }
-  }
+  };
 }
