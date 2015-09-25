17860bca51b0e349ab880002cabf0306987a0362
Martin Jantosovic <jantosovic.martin@gmail.com>
2015-09-25 09:19:27 +0200

    chore(babel): Switch to babel instead of traceur

--- config.js
+++ config.js
@@ -1,19 +1,34 @@
 System.config({
-  "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime",
+      "es7.decorators"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
     "aurelia-sails-socket-client/*": "dist/system/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
-  }
-});
+    "npm:*": "jspm_packages/npm/*"
+  },
 
-System.config({
-  "map": {
+  map: {
     "aurelia-path": "github:aurelia/path@0.9.0",
-    "core-js": "github:zloirock/core-js@1.0.1",
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "core-js": "npm:core-js@1.1.4",
     "sails.io.js": "github:balderdashy/sails.io.js@0.11.5",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.8.25": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.1.4": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
   }
 });
-
